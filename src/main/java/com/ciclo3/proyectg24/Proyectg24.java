983bdee5d171ce69db4b6d91d4ceb6e0e2693d1f
stiver smalbach bolaño <stiver.smalbach.mt@usa.edu.co>
2022-09-16 16:01:08 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/ciclo3/proyectg24/Proyectg24.java
@@ -0,0 +1,11 @@
+
+
+package com.ciclo3.proyectg24;
+
+
+public class Proyectg24 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/ciclo3/proyectg24/ejercicio1.java
@@ -0,0 +1,11 @@
+
+package com.ciclo3.proyectg24;
+
+
+public class ejercicio1 {
+    
+    public void metodo(){
+        System.out.println("Hello World!");
+    }
+    
+}
